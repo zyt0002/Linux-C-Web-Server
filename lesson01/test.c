eee0d1d31a8c1f162f7fb718e8b1a2d9df5c6ab4
TaoShao <lori8@vip.qq.com>
2022-12-08 23:17:39 +0800

    Output of L7

--- lesson01/test.c
+++ lesson01/test.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
 #define PI 3.14
 int main(){
     int i = PI +10;
     printf("hello");
+    while(1){
+        printf("world\n");
+        sleep(1);
+    }
     return 0;
 }
